9cf991bde90747e9f22598d9ae9d1520dbf9dd07
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2019-02-03 11:56:15 +1100

    Add robot controls

--- robot.py
+++ robot.py
@@ -81,10 +81,7 @@ class Robot(magicbot.MagicRobot):
         self.imu = NavX()
         self.vision = Vision()
 
-        self.sd = NetworkTables.getTable("SmartDashboard")
-        wpilib.SmartDashboard.putData("Gyro", self.imu.ahrs)
-
-        # hatch objects
+        # Hatch objects
         self.hatch_top_puncher = wpilib.Solenoid(0)
         self.hatch_left_puncher = wpilib.Solenoid(1)
         self.hatch_right_puncher = wpilib.Solenoid(2)
@@ -92,22 +89,27 @@ class Robot(magicbot.MagicRobot):
         self.hatch_top_limit_switch = wpilib.DigitalInput(1)
         self.hatch_left_limit_switch = wpilib.DigitalInput(2)
         self.hatch_right_limit_switch = wpilib.DigitalInput(3)
-        # self.front_lift_motor = rev.CANSparkMax(0, rev.MotorType.kBrushless)
-        self.front_lift_limit_switch = wpilib.DigitalInput(4)
 
-        # self.back_lift_motor = rev.CANSparkMax(1, rev.MotorType.kBrushless)
-        self.back_lift_limit_switch = wpilib.DigitalInput(5)
-
-        self.lift_drive_motor = ctre.TalonSRX(20)
-
-        # cargo related objects
+        # Cargo objects
         self.intake_motor = ctre.TalonSRX(9)
         self.intake_switch = wpilib.DigitalInput(0)
 
-        # boilerplate setup for the joystick
-        self.joystick = wpilib.Joystick(0)
+        # Lift
+        self.lift_drive_motor = ctre.TalonSRX(20)
+        # self.front_lift_motor = rev.CANSparkMax(0, rev.MotorType.kBrushless)
+        self.front_lift_limit_switch = wpilib.DigitalInput(4)
+        # self.back_lift_motor = rev.CANSparkMax(1, rev.MotorType.kBrushless)
+        self.back_lift_limit_switch = wpilib.DigitalInput(5)
 
+        # Controlers
+        self.joystick = wpilib.Joystick(1)
+        self.gamepad = wpilib.XboxController(0)
+
+        # Controller related variables
         self.spin_rate = 1.5
+
+        self.sd = NetworkTables.getTable("SmartDashboard")
+        wpilib.SmartDashboard.putData("Gyro", self.imu.ahrs)
 
     def disabledPeriodic(self):
         self.chassis.set_inputs(0, 0, 0)
@@ -119,16 +121,70 @@ class Robot(magicbot.MagicRobot):
 
     def teleopPeriodic(self):
         """Allow the drivers to control the robot."""
-        # self.chassis.heading_hold_off()
-
-        throttle = (1 - self.joystick.getThrottle()) / 2
-        self.sd.putNumber("joy_throttle", throttle)
-
         # this is where the joystick inputs get converted to numbers that are sent
         # to the chassis component. we rescale them using the rescale_js function,
         # in order to make their response exponential, and to set a dead zone -
         # which just means if it is under a certain value a 0 will be sent
         # TODO: Tune these constants for whatever robot they are on
+
+        # self.chassis.heading_hold_off()
+
+        # Handle co-driver input
+        # Intaking (hatch and cargo)
+        if self.gamepad.getAButtonPressed():
+            self.hatchman.punch(force=True)
+        if self.gamepad.getBButtonPressed():
+            self.cargo.intake_depot(force=True)
+        if self.gamepad.getXButtonPressed():
+            self.hatch.intake(force=True)  # might remove
+        if self.gamepad.getYButtonPressed():
+            self.cargo.intake_loading(force=True)
+
+        # Cargo (arm)
+        x = rescale_js(self.gamepad.getX(self.gamepad.Hand.kLeft), deadzone=0.5)
+        y = rescale_js(self.gamepad.getY(self.gamepad.Hand.kLeft), deadzone=0.5)
+        if x != 0.0 or y != 0.0:
+            direction = math.degrees(math.atan2(y, x))
+            if 45 < direction < 135:
+                self.arm.lift()
+            elif -135 < direction < -45:
+                self.arm.lower()
+
+        # Outaking (hatch and cargo)
+        if (
+            self.gamepad.getTriggerAxis(self.gamepad.Hand.kLeft) > 0
+            or self.gamepad.getTriggerAxis(self.gamepad.Hand.kRight) > 0
+        ):
+            if self.intake.contained():
+                if not self.cargo.is_executing:
+                    self.cargo.override = self.gamepad.getBumper(
+                        self.gamepad.Hand.kLeft
+                    ) or self.gamepad.getBumper(self.gamepad.Hand.kRight)
+                    self.cargo.start_outtake(force=True)
+            elif self.intake.contained():
+                if not self.hatch.is_executing:
+                    self.hatch.override = self.gamepad.getBumper(
+                        self.gamepad.Hand.kLeft
+                    ) or self.gamepad.getBumper(self.gamepad.Hand.kRight)
+                    self.hatch.outtake(force=True)
+
+        # Climbing
+        if self.gamepad.getStartButtonPressed():
+            self.climb_automation.climb(force=True)
+        if self.gamepad.getBackButtonPressed():
+            self.climb_automation.reset(force=True)
+
+        # Snap to angle
+        gamepad_hat = self.gamepad.getPOV()
+        if gamepad_hat != -1:
+            constrained_angle = -constrain_angle(math.radians(gamepad_hat))
+            self.chassis.set_heading_sp(constrained_angle)
+
+        # Handle driver input
+        # Driving
+        throttle = (
+            1 - self.joystick.getThrottle()
+        ) / 2  # TODO: don't set to 0 when not turned on
         joystick_vx = -rescale_js(
             self.joystick.getY(), deadzone=0.1, exponential=1.5, rate=4 * throttle
         )
