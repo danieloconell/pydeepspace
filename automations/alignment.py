a3fb34a2d8af82da53861d2178c845fdd814312d
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2019-02-18 19:36:49 +1100

    Update cargo code
    - Remove heights not being used
    - Use sparkmaxes for arm motor
    - Use victor spx for intake motor

--- automations/alignment.py
+++ automations/alignment.py
@@ -4,7 +4,7 @@ from magicbot import tunable
 from magicbot.state_machine import StateMachine, state
 
 from components.hatch import Hatch
-from components.cargo import Intake
+from components.cargo import CargoManipulator
 from components.vision import Vision
 from pyswervedrive.chassis import SwerveChassis
 
@@ -89,12 +89,12 @@ class HatchDepositAligner(Aligner):
 class CargoDepositAligner(Aligner):
 
     VERBOSE_LOGGING = True
-    intake: Intake
     ROBOT_SIDE = Sides.BACK
+    cargo_component: CargoManipulator
 
     @state(must_finish=True)
     def success(self):
-        self.intake.deposit()
+        self.cargo_component.outtake()
         self.done()
 
 
